76764838a8625cbe2fe3b523bac2f2158c5ac6e9
David <crusedmoss@gmail.com>
2023-03-09 13:10:27 -0600

    challenge improvements

--- dpk.test.js
+++ dpk.test.js
@@ -19,4 +19,20 @@ describe("deterministicPartitionKey", () => {
       crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex")
     );
   });
+
+  it("Return event partitionkey as String", () => {
+    const event = { partitionKey: 1 };
+    expect(deterministicPartitionKey(event)).toBe("1");
+  });
+
+  it("Return hashed candidate", () => {
+    const event = {
+      partitionKey:
+        "oclFmYsZVKEI4OrnxNHX9QWedBaiCCkWh4RdX4FyUALYsi40F63pYspfpN9mrCV30HuZhwQ44jrtgvaaeQBF0omoEgnT7kDjP1hmROgZlD6s9Flc9LeBGNMlBi5nWbzhT3kc5uEbEZIGhMDrHjtT2qbQA0CJYoXLy6D6hI56TbPX4nMQxIItCFx7yndyUMshHRWu6UZjEqepTfvxUeoQYsZIke5RwJWufs8tJAcJWW9k5eyiWUkhErblVyMR477Pn",
+    };
+
+    expect(deterministicPartitionKey(event)).toBe(
+      crypto.createHash("sha3-512").update(event.partitionKey).digest("hex")
+    );
+  });
 });
